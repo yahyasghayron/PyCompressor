1060f41d5e23556c6e5c2ffcaf756830a6ff7dcb
yahya sghayron <yahya.sghayron123@gmail.com>
2024-08-01 00:10:39 +0100

    init huffman

---
+++ src/huffman.py
@@ -0,0 +1,14 @@
+def compress(data):
+    """Compress input data using Huffman coding."""
+    # Your code here
+    return data
+
+
+def decompress(data):
+    """Decompress input data using Huffman coding."""
+    # Your code here
+    return data
+
+if __name__ == "__main__":
+    # Test your implementation here
+    pass
